2c588fd0be8c1cd3139a1c0897f3bf15cd4bd1c9
CODE TỐT <tech.codetot@gmail.com>
2021-09-16 14:33:27 +0700

    Create example-block.php

---
+++ wp-child-theme/blocks/example-block.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Teacher - Right Image
+ *
+ * PHP Version 7.0
+ *
+ * @category CT_Block
+ * @package  Vietmoz
+ * @author   CODE TOT JSC <dev@codetot.com>
+ * @license  GPL v2+ https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ * @link     https://codetot.com
+ * @since    1.0.0
+ */
+
